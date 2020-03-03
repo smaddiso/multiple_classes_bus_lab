6a087f3dcf9f99c939c8376d0fd80533b61aa2b2
Steve Maddison <stevemaddison@Steves-MacBook-Pro.local>
2020-03-03 16:02:07 +0000

    Part B

--- bus.rb
+++ bus.rb
@@ -1,36 +1,28 @@
 class Bus
 
-attr_reader :number, :destination
+attr_accessor :number, :destination, :passengers
 
-def initialize(number, destination)
+def initialize(number, destination, passengers)
   @number = number
   @destination = destination
   @drive = "Brum Brum"
+  @passengers = []
 end
 
+  def number_of_passengers()
+    return @passengers.length
+  end
 
+  def add_passenger(new_passenger)
+    @passengers.push(new_passenger)
+  end
 
+  def remove_passenger(old_passenger)
+    @passengers.delete(old_passenger)
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def empty()
+    @passengers.clear()
+  end
 
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -3,11 +3,12 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative("../bus")
+require_relative("../person")
 
 class TestBus < MiniTest::Test
 
   def setup()
-    @bus = Bus.new(42, "Raigmore Hospital")
+    @bus = Bus.new(42, "Raigmore Hospital", [])
   end
 
   def test_bus_has_number()
@@ -18,21 +19,25 @@ class TestBus < MiniTest::Test
     assert_equal("Raigmore Hospital", @bus.destination)
   end
 
+  def test_number_of_passengers()
+    assert_equal(0, @bus.number_of_passengers())
+  end
 
+  def test_pick_up_passenger()
+    new_passenger = Person.new("Stella", 72)
+    @bus.add_passenger(new_passenger)
+    assert_equal(1, @bus.number_of_passengers())
+  end
 
+  def test_drop_off_passenger()
+    old_passenger = Person.new("Stella", 72)
+    @bus.remove_passenger(old_passenger)
+    assert_equal(0, @bus.number_of_passengers())
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def test_empty_bus()
+    @bus.empty()
+    assert_equal(0, @bus.number_of_passengers())
+  end
 
 end
